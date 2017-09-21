3b10f8d718022f601e2d9938274bc4fbbb83a82a
pihanhsu <pihanhsu@gmail.com>
2017-09-21 15:53:27 +0800

    create player and team model

---
+++ myscoreboard_lite/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  myscoreboard_lite
+//
+//  Created by PiHan on 2017/9/21.
+//  Copyright © 2017年 PiHan. All rights reserved.
+//
+
+import Foundation
+struct Player {
+    let name: String
+    let imageUrl: String
+    var uWeight: Int
+}
---
+++ myscoreboard_lite/Model/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  myscoreboard_lite
+//
+//  Created by PiHan on 2017/9/21.
+//  Copyright © 2017年 PiHan. All rights reserved.
+//
+
+import Foundation
+struct Team {
+    let players = [Player]()
+    let name: String
+    
+}
