a9292b8cacb9efc8be0a60686f096031042548ba
pihanhsu <pihanhsu@gmail.com>
2017-10-13 03:15:00 +0800

    add validate() when calling log out api

--- myscoreboard_lite/Controller/TeamTableViewController.swift
+++ myscoreboard_lite/Controller/TeamTableViewController.swift
@@ -65,7 +65,7 @@ class TeamTableViewController: UITableViewController {
     @IBAction func logout(_ sender: Any) {
         let params = ["auth_token" : DataSource.sharedInstance.auth_token]
         
-        Alamofire.request("https://product.myscoreboardapp.com/api/v1/logout", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON{response in
+        Alamofire.request("https://product.myscoreboardapp.com/api/v1/logout", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate().responseJSON{response in
             if response.result.isSuccess{
                 DataSource.sharedInstance = DataSource()
                 self.navigationController?.popViewController(animated: true)
@@ -77,43 +77,7 @@ class TeamTableViewController: UITableViewController {
         
     }
     
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
