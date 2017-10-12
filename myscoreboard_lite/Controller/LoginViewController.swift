e12465c93e3b87d6a943c03bafa9b2058affd8eb
pihanhsu <pihanhsu@gmail.com>
2017-10-12 22:08:28 +0800

    add indicator in login page, add validate() when calling api

--- myscoreboard_lite/Controller/LoginViewController.swift
+++ myscoreboard_lite/Controller/LoginViewController.swift
@@ -14,6 +14,8 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var loginButton: CapsuleButton!
     
     var user: Player?
     
@@ -31,25 +33,36 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginButtonPressed(_ sender: Any) {
         
+        activityIndicator.startAnimating()
+        loginButton.isEnabled = false
+        
         let email = emailTextField.text!.trim()
         let password = passwordTextField.text!.trim()
         
         let params = ["email" : email,
                   "password" : password]
-        Alamofire.request("https://product.myscoreboardapp.com/api/v1/login", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON{response in
+        Alamofire.request("https://product.myscoreboardapp.com/api/v1/login", method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).validate().responseJSON{response in
+           
             if response.result.isSuccess{
+                
                 let player = JSON(response.result.value!)
                
                 if let user_id = player["user_id"].int {
                     if let auth_token = player["auth_token"].string{
                         self.getTeamList(user_id, token: auth_token)
-                        DataSource.sharedInstance.auth_token = auth_token
+                DataSource.sharedInstance.auth_token = auth_token
                     }
-                    
                 }
             }else{
-                print("can't login")
+                let alertController = UIAlertController(title: "登入失敗", message: "請確認帳號密碼是否正確", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+                    self.activityIndicator.stopAnimating()
+                })
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+               
             }
+            self.loginButton.isEnabled = true
         }
     }
     
@@ -57,30 +70,22 @@ class LoginViewController: UIViewController {
         
         let params = ["auth_token" : token,
                       "user_id" : userID] as [String : Any]
-        Alamofire.request("https://product.myscoreboardapp.com/api/v1/teams", method: .get, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON{response in
+        Alamofire.request("https://product.myscoreboardapp.com/api/v1/teams", method: .get, parameters: params, encoding: JSONEncoding.default, headers: nil).validate().responseJSON{response in
             if response.result.isSuccess{
+                
                 let data = JSON(response.result.value!)
                 
-                print("success")
-                //print(data)
                 for team in data["results"].arrayValue {
                     DataSource.sharedInstance.teams.append(Team(data: team))
                 }
-                
+                self.activityIndicator.stopAnimating()
                 self.performSegue(withIdentifier: "GoToTeamsPage", sender: self)
             }else{
-                print("error")
+                
+                print("load team data error")
             }
+            self.loginButton.isEnabled = true
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- myscoreboard_lite/View/CapsuleButton.swift
+++ myscoreboard_lite/View/CapsuleButton.swift
@@ -14,12 +14,5 @@ class CapsuleButton: UIButton, Capsule, DropShadow {
         self.createCapsuleOutline()
         self.addDropShadow()
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
