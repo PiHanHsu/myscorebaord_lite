357e7a0c2575d2e1ea61b6240268f147ddef2d9d
pihanhsu <pihanhsu@gmail.com>
2017-10-24 22:45:19 +0800

    Add cancel button on TeamVC

--- myscoreboard_lite/Controller/TeamViewController.swift
+++ myscoreboard_lite/Controller/TeamViewController.swift
@@ -26,20 +26,8 @@ class TeamViewController: UIViewController {
         teamNameTextField.text = team?.name
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
